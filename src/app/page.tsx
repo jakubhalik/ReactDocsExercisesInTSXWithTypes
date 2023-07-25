fad116f5e64d57b4defb84b99dfcbc6280b9463f
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-07-25 16:42:57 +0200

    ""

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import './globals.css'; import Styles from './Styles'; import ToDo from './ToDo'; import PropsToCompExample from './PropsToCompExample'; 
 import ConditionalRenderingExample from './ConditionalRenderingExample'; import RenderingListsExample from './RenderingListsExample';
-type Component = { [key: string]: React.FC; };
-const Components: Component = { ToDo, PropsToCompExample, ConditionalRenderingExample, RenderingListsExample }
+type Component = { [key: string]: React.FC; }; const Components: Component = { ToDo, PropsToCompExample, ConditionalRenderingExample, RenderingListsExample }
 export default function App() { return Object.keys(Components).map(name => { const Component = Components[name]; return <Styles key={name} name={name}><Component /></Styles> }); }
