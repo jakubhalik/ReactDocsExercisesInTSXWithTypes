8b733ff9e8882a157a10d9a3ba8f1eae9d8ffcbc
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-07-30 14:10:41 +0200

    "pythonscriptbrevity"

--- generateIndex.py
+++ generateIndex.py
@@ -1,5 +1,2 @@
-import os; directory_path = './src/app'
-components = [ 'TaskList', 'PropsToCompExample', 'ConditionalRenderingExample', 'RenderingListsExample', 'PureComponent' ]
-export_statements = '\n'.join([f'export {{ default as { component } }} from \'./{component}\';' for component in components])
-index_path = os.path.join(directory_path, 'index.ts')
-with open(index_path, 'w') as index_file: index_file.write(export_statements); print('Export statements written in index.ts')
+open('./src/app/index.ts', 'w').write(''.join(map(lambda x:f'export {{ default as {x} }} from \'./{x}\';',
+    ['TaskList', 'PropsToCompExample', 'ConditionalRenderingExample', 'RenderingListsExample', 'PureComponent'])))
--- generateIndex.ts
+++ generateIndex.ts
@@ -1,4 +1,4 @@
 const fs = require('fs'); const path = require('path'); const directoryPath = './src/app';
 const components = [ 'TaskList', 'PropsToCompExample', 'ConditionalRenderingExample', 'RenderingListsExample', 'PureComponent' ];
 const exportStatements = components.map(component => `export { default as ${component} } from './${component}';`).join('\n'); 
-const indexPath = path.resolve(directoryPath, 'index.ts'); fs.writeFileSync(indexPath, exportStatements); console.log('Export statements written to index.ts');
+const indexPath = path.resolve(directoryPath, 'index.ts'); fs.writeFileSync(indexPath, exportStatements);
